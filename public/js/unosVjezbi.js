7637138d3ee568fdcdb3c06e1ab122144402fa84
mmarincic1 <mmarincic1@etf.unsa.ba>
2022-01-10 12:34:56 +0100

    neki detalji za unos realnih brojeva u formu

--- public/js/unosVjezbi.js
+++ public/js/unosVjezbi.js
@@ -1,28 +1,40 @@
-function dodajInputPolja(){
+function dodajInputPolja() {
     var x = document.getElementById("unosZadataka");
     var y = document.getElementById("posaljiZadatke");
     x.style.display = "block";
-
-    if(document.getElementById("brojVjezbi").value <= 0){
+    let brVj = parseFloat(document.getElementById("brojVjezbi").value);
+    if (brVj != parseInt(brVj)) {
         x.style.display = "none";
-    }
-    
-    VjezbeAjax.dodajInputPolja(document.getElementById("unosZadataka"), document.getElementById("brojVjezbi").value);
-    y.style.display = "block";
-    if(document.getElementById("brojVjezbi").value <= 0){
         y.style.display = "none";
+        console.log("Nevalidan broj vjezbi!");
+    }
+    else {
+
+        if (document.getElementById("brojVjezbi").value <= 0) {
+            x.style.display = "none";
+        }
+
+        VjezbeAjax.dodajInputPolja(document.getElementById("unosZadataka"), document.getElementById("brojVjezbi").value);
+        y.style.display = "block";
+        if (document.getElementById("brojVjezbi").value <= 0) {
+            y.style.display = "none";
+        }
     }
 }
 
-function posaljiZadatke(){
+function posaljiZadatke() {
     var json = "{\"brojVjezbi\":" + document.getElementById("brojVjezbi").value + ",\"brojZadataka\":[";
-    for(let i = 0; i < document.getElementById("brojVjezbi").value; i++){
-        json+= document.getElementById("z" + i).value + ",";
+    for (let i = 0; i < document.getElementById("brojVjezbi").value; i++) {
+        if(parseFloat(document.getElementById("z" + i).value) != parseInt(document.getElementById("z" + i).value)){
+            console.log("Pogrešan broj zadatka vjezbe " +(i+1));
+            return;
+        }
+        json += document.getElementById("z" + i).value + ",";
     }
-    json = json.substring(0, json.length-1);
-    json+="]}";
+    json = json.substring(0, json.length - 1);
+    json += "]}";
     VjezbeAjax.posaljiPodatke(JSON.parse(json), function (err, data) {
-        if(err)
+        if (err)
             console.log(err);
         else console.log(data);
     });
--- public/js/vjezbeAjax.js
+++ public/js/vjezbeAjax.js
@@ -2,7 +2,7 @@ let VjezbeAjax = (function () {
     const dodajInputPolja = function (DOMelementDIVauFormi, brojVjezbi) {
         let noveLabele = "";
         for (let i = 0; i < brojVjezbi; i++) {
-            noveLabele += "<label for=z" + i + ">Broj zadataka za Vježbu " + (i + 1) + ":</label><input type=\"number\" id=z" + i + " class=\"inputText\" value=4 name=z" + i + ">"
+            noveLabele += "<label for=z" + i + ">Broj zadataka za Vježbu " + (i + 1) + ":</label><input type=\"number\" id=z" + i + " class=\"inputText\" value=4 name=z" + i + " min=0 max=10>"
         }
         DOMelementDIVauFormi.innerHTML = noveLabele;
     }
