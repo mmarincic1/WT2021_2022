5f3a1237f890b50fa1414b7d6c7e67ec6d6d3bfc
mmarincic1 <mmarincic1@etf.unsa.ba>
2022-01-17 00:27:15 +0100

    popravljeno da password baze bude password

--- baza.js
+++ baza.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("wt2118729", "root", "", {
+const sequelize = new Sequelize("wt2118729", "root", "password", {
     host: "localhost",
     dialect: "mysql"
 });
